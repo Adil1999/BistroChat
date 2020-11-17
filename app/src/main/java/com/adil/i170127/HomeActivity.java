65682dd59714aaac242a43c3868a951a11ef9635
Adil Alam <alamadil540@gmail.com>
2020-11-17 12:29:52 +0500

    Added drawer layout

--- app/src/main/java/com/adil/i170127/HomeActivity.java
+++ app/src/main/java/com/adil/i170127/HomeActivity.java
@@ -1,61 +1,185 @@
 package com.adil.i170127;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.nfc.cardemulation.HostNfcFService;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class HomeActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    StorageReference storageReference;
     FirebaseUser user;
+
     Toolbar toolbar;
-    TextView title;
+    DrawerLayout dl;
+    ActionBarDrawerToggle t;
+    NavigationView nv;
+
+    TextView title,nav_email, nav_name;
+    CircleImageView civ;
+    User userData;
+
+    public void nav_logout(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+        Toast.makeText(HomeActivity.this, "Logout",Toast.LENGTH_SHORT).show();
+    }
+
+    public void set_navigation(){
+        nv.setItemIconTintList(null);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch(id)
+                {
+                    case R.id.logout:
+                        nav_logout();
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+
+            }
+        });
+    }
+
+    public void retrieve_user(){
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    userData = ds.getValue(User.class);
+                    nav_name.setText(userData.getFname() + " " + userData.getLname());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(HomeActivity.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void set_navHeaderFields(){
+        nav_email.setText(user.getEmail());
+        storageReference.child(user.getUid()).getDownloadUrl()
+            .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Picasso.get().load(uri).fit().centerCrop().into(civ);
+                }
+            });
+    }
+
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_home);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference().child(user.getUid());
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         toolbar = findViewById(R.id.myAppBar);
+        dl = (DrawerLayout)findViewById(R.id.nav_drawer);
+        nv = (NavigationView)findViewById(R.id.nv);
+        View header=nv.getHeaderView(0);
+        civ = header.findViewById(R.id.profile);
+        nav_email = header.findViewById(R.id.email);
+        nav_name = header.findViewById(R.id.name);
         title = toolbar.findViewById(R.id.title);
-        setSupportActionBar(toolbar);
 
         title.setText("BistroChat");
         toolbar.setNavigationIcon(R.drawable.ic_baseline_menu_24);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, MainActivity.class));
-            }
-        });
+        t = new ActionBarDrawerToggle(this, dl, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dl.addDrawerListener(t);
+        t.setDrawerIndicatorEnabled(true);
+        t.syncState();
 
+        set_navigation();
+        retrieve_user();
+        set_navHeaderFields();
     }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        user = firebaseAuth.getCurrentUser();
-//        if(user != null){
-//            //Toast.makeText(HomeActivity.this, "User is Logged in "+user.getEmail(), Toast.LENGTH_LONG).show();
-//        } else {
-//            startActivity(new Intent(HomeActivity.this, MainActivity.class));
-//            finish();
-//        }
-//    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        t.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        t.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+
+        return true;
+    }
 }
